2a85136f65dda1bf202cd9a37eeaf5c69656c916
wenqiang <wenqiang9946@163.com>
2023-08-16 12:40:28 +0800

    feat: 十进制转化为二进制

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"Golang/channel"
+	"Golang/usual"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", channel.OverTimeControl)
 	http.ListenAndServe("127.0.0.1:8080", nil)
+	fmt.Print(usual.ConvertBin(8))
 }
---
+++ usual/convertTobin.go
@@ -0,0 +1,12 @@
+package usual
+
+import "strconv"
+
+func ConvertBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
